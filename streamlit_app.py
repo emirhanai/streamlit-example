e7f67ab8fb2afc5ab6460e02d83669e8760daacf
Emirhan BULUT <72056400+emirhanai@users.noreply.github.com>
2022-12-28 23:22:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,9 +125,21 @@ if __name__ == "__main__":
     usernames = ['jsmith', 'rbriggs']
     passwords = ['123', '456']
     hashed_passwords = stauth.hasher(passwords).generate()
-    authenticator = stauth.authenticate(names, usernames, hashed_passwords,
-                                        'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
+    credentials = {
+        "usernames":{
+            usernames[0]:{
+                "name":names[0],
+                "password":passwords[0]
+                },
+            usernames[1]:{
+                "name":names[1],
+                "password":passwords[1]
+                }            
+            }
+        }
+    authenticator = stauth.Authenticate(credentials, "app_home", "auth", cookie_expiry_days=30)
     name, authentication_status = authenticator.login('Login', 'main')
+    
     if st.session_state["authentication_status"]:
         authenticator.logout('Logout', 'main')
         st.write(f'Welcome *{st.session_state["name"]}*')
